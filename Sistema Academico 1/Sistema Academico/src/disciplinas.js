bc979ac76f686abb6f1aa42ee0e5cd7fe21d2792
jukalbf <lgbr212@gmail.com>
2025-10-09 20:41:04 -0300

    fix: arrumando conflito de branch

--- Sistema Academico 1/Sistema Academico/src/disciplinas.js
+++ Sistema Academico 1/Sistema Academico/src/disciplinas.js
@@ -1,13 +1,34 @@
-const calcularMedia = (n1, n2) => (n1 + n2) / 2;
+function calcularMedia(nota1, nota2) {
+  try {
+    if (nota1 < 0  nota2 < 0) {
+        console.error("As notas não podem ser negativas"); 
+    }
 
-const exibirStatus = (media) => {
-  if (media < 3) {
-    return "Reprovado por média";
-  } else if (media >= 3 && media < 7) {
-    return "Aguardando a final";
-  } else {
-    return "Aprovado por média";
-  }
-};
+    return (nota1 + nota2) /2;
+
+  } catch (error) {
+     console.error("Erro ao calcular a média:", error.message);
+    }
+ }
+
+function exibirStatus(media) {
+  try {
+    if (media < 0  media > 10) {
+      console.error("A média deve estar entre 0 e 10");
+    }
+
+      if(media < 3) {
+       return "Aluno Reprovado, pois ficou abaixo da média";
+      } else if (media >= 3 && media < 7) {
+        return "Aguardando a final";
+      } else {
+        return "Aprovado por média";
+      }
+
+    } catch(error) {
+        console.error("Erro ao exibir média do aluno, contate o suporte de TI da faculdade", error.message);
+   };
+   return "Erro ao calcular status";
+ }
 
 module.exports = { calcularMedia, exibirStatus };
--- Sistema Academico 1/Sistema Academico/src/pagamento.js
+++ Sistema Academico 1/Sistema Academico/src/pagamento.js
@@ -1,10 +1,21 @@
 const { PRECO_MENSALIDADE, DESCONTO_ATE_DIA_5 } = require("./constantes");
 
-const calcularValorDaMensalidade = (dia) => {
-  if (dia <= 5) {
-    return PRECO_MENSALIDADE - PRECO_MENSALIDADE * DESCONTO_ATE_DIA_5;
+function calcularValorDaMensalidade(dia) {
+  try {
+    if (typeof dia !== "number") {
+      return "Dia de pagamento inválido";
+    }
+
+    if (dia <= 5) {
+      return PRECO_MENSALIDADE - PRECO_MENSALIDADE * DESCONTO_ATE_DIA_5;
+    }
+
+    return PRECO_MENSALIDADE;
+
+  } catch(error) {
+    console.error("Erro ao realizar pagamento, contate o financeiro da faculdade", error.message);
+    return "Erro no cálculo da mensalidade";
   }
-  return PRECO_MENSALIDADE;
-};
+}
 
 module.exports = { calcularValorDaMensalidade };
