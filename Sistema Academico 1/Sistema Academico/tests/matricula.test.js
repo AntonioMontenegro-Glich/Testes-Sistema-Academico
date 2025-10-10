fe5473265b545320b114b304db6acff718941903
jukalbf <lgbr212@gmail.com>
2025-10-09 22:11:48 -0300

    test(matricula.test.js): colocando testes de limite de alunos dentro de um describe

--- Sistema Academico 1/Sistema Academico/tests/matricula.test.js
+++ Sistema Academico 1/Sistema Academico/tests/matricula.test.js
@@ -2,25 +2,27 @@ import { LIMITE_ALUNOS_ABERTURA_TURMA } from "../src/constantes.js";
 import { podeAbrirNovaTurma } from "../src/matricula.js";
 
 describe("Função podeAbrirNovaTurma", () => {
-  test("deve retornar false quando há menos alunos que o limite", () => {
-    const listaAlunos = [{nome: "Aluno1"}, {nome: "Aluno2"}, {nome: "Aluno3"}, {nome: "Aluno4"}];
-    expect(podeAbrirNovaTurma(listaAlunos)).toBe(false);
-  });
+  describe("Limite de alunos", () => {
+    test("deve retornar false quando há menos alunos que o limite", () => {
+      const listaAlunos = [{nome: "Aluno1"}, {nome: "Aluno2"}, {nome: "Aluno3"}, {nome: "Aluno4"}];
+      expect(podeAbrirNovaTurma(listaAlunos)).toBe(false);
+    });
 
-  test("deve retornar true quando há alunos suficientes", () => {
-    const listaAlunos = [
-      {nome: "Aluno1"}, {nome: "Aluno2"}, {nome: "Aluno3"}, 
-      {nome: "Aluno4"}, {nome: "Aluno5"}
-    ];
-    expect(podeAbrirNovaTurma(listaAlunos)).toBe(true);
-  });
+    test("deve retornar true quando há alunos suficientes", () => {
+      const listaAlunos = [
+        {nome: "Aluno1"}, {nome: "Aluno2"}, {nome: "Aluno3"}, 
+        {nome: "Aluno4"}, {nome: "Aluno5"}
+      ];
+      expect(podeAbrirNovaTurma(listaAlunos)).toBe(true);
+    });
 
-  test("deve retornar true quando há mais alunos que o limite", () => {
-    const listaAlunos = [
-      {nome: "Aluno1"}, {nome: "Aluno2"}, {nome: "Aluno3"},
-      {nome: "Aluno4"}, {nome: "Aluno5"}, {nome: "Aluno6"}
-    ];
-    expect(podeAbrirNovaTurma(listaAlunos)).toBe(true);
-  });
+    test("deve retornar true quando há mais alunos que o limite", () => {
+      const listaAlunos = [
+        {nome: "Aluno1"}, {nome: "Aluno2"}, {nome: "Aluno3"},
+        {nome: "Aluno4"}, {nome: "Aluno5"}, {nome: "Aluno6"}
+      ];
+      expect(podeAbrirNovaTurma(listaAlunos)).toBe(true);
+    });
+  })
 });
 
