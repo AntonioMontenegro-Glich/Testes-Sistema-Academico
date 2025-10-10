97ac82c16297acf584c0e3f946fc45920659f92f
Antonio-montenegro-glich <antoniolemos2004@gmail.com>
2025-10-09 22:50:12 -0300

    ajustando disciplina.js

--- Sistema Academico 1/Sistema Academico/src/disciplinas.js
+++ Sistema Academico 1/Sistema Academico/src/disciplinas.js
@@ -1,38 +1,36 @@
 function calcularMedia(nota1, nota2) {
   try {
     if (nota1 < 0 || nota2 < 0) {
-        console.error("As notas não podem ser negativas"); 
+      throw new Error("As notas não podem ser negativas");
     }
 
-    return (nota1 + nota2) /2;
+    return (nota1 + nota2) / 2;
 
   } catch (error) {
-     console.error("Erro ao calcular a média:", error.message);
-    }
- }
+    throw new Error("Erro ao calcular a média:", error.message);
+  }
+}
 
 function exibirStatus(media) {
   try {
-    if(typeof media !== "number) {
-        console.error("Tipo de valor não válido")
-       }
-    
-    if (media < 0 || media > 10) {
-      console.error("A média deve estar entre 0 e 10");
+    if (typeof media !== "number") {
+      throw new Error("Tipo de valor não válido")
     }
 
-      if(media < 3) {
-       return "Aluno Reprovado, pois ficou abaixo da média";
-      } else if (media >= 3 && media < 7) {
-        return "Aguardando a final";
-      } else {
-        return "Aprovado por média";
-      }
+    if (media < 0 || media > 10) {
+      throw new Error("A média deve estar entre 0 e 10");
+    }
 
-    } catch(error) {
-        console.error("Erro ao exibir média do aluno, contate o suporte de TI da faculdade", error.message);
-   };
-   return "Erro ao calcular status";
- }
+    if (media < 3) {
+      return "Aluno Reprovado, pois ficou abaixo da média";
+    } else if (media >= 3 && media < 7) {
+      return "Aguardando a final";
+    } else {
+      return "Aprovado por média";
+    }
+  } catch (error) {
+    throw new Error("Erro ao exibir média do aluno, contate o suporte de TI da faculdade", error.message);
+  }
+}
 
 module.exports = { calcularMedia, exibirStatus };
